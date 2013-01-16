08358cd4fd7cafb9695416562b1366637de517e9
Sander van de Graaf <mail@svdgraaf.nl>
2013-01-16 16:51:41 +0100

    rename to represent API better

--- tapatalk/post.py
+++ tapatalk/post.py
@@ -1,13 +1,14 @@
 from djangobb_forum.models import *
 
-def get_thread(topic_id, start_num, last_num, return_html=False):
+def get_thread(topic_id, start_num, last_num, return_html=True):
     topic = Topic.objects.get(pk=topic_id)
+    print topic
 
     data = {
         'total_post_num': topic.post_count,
-        'forum_id': topic.forum.id,
+        'forum_id': str(topic.forum.id),
         'forum_name': topic.forum.name,
-        'topic_id': topic.id,
+        'topic_id': str(topic.id),
         'topic_title': topic.name,
         'posts': [],
     }
@@ -15,10 +16,10 @@ def get_thread(topic_id, start_num, last_num, return_html=False):
     posts = Post.objects.filter(topic=topic)
     for post in posts:
         p = {
-            'post_id': post.id,
+            'post_id': str(post.id),
             'post_title': '',
             'post_content': post.body,
-            'post_author_id': post.user.id,
+            'post_author_id': str(post.user.id),
             'post_author_name': post.user.username,
             'post_time': post.created.isoformat(),
         }
