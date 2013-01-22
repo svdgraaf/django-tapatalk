03203aabf169ed1a94abefffe70c570f3a4a3e6d
Sander van de Graaf <mail@svdgraaf.nl>
2013-01-22 15:06:49 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+# try:
+#     README = open('README.md').read()
+# except:
+#     README = None
+
+# try: 
+#     LICENSE = open('LICENSE.txt').read()
+# except: 
+#     LICENSE = None
+#     
+README = ''
+LICENSE = ''
+
+setup(
+    name = 'django-tapatalk',
+    version = '0.0.1',
+    description='DjangoBB Tapatalk implementation',
+    long_description=README,
+    author = 'Sander van de Graaf',
+    author_email = 'mail@svdgraaf.nl',
+    license = LICENSE,
+    url = 'http://github.com/svdgraaf/django-tapatalk/',
+    packages = find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Environment :: Web Environment',
+    ],
+)
