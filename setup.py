c221a78c9de7027991e92704080ad22ab3085212
Sander van de Graaf <mail@svdgraaf.nl>
2013-01-22 16:56:25 +0100

    new revision

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ LICENSE = ''
 
 setup(
     name = 'django-tapatalk',
-    version = '0.0.1',
+    version = '0.0.2',
     description='DjangoBB Tapatalk implementation',
     long_description=README,
     author = 'Sander van de Graaf',
