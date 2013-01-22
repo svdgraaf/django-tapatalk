daaef03f1aef770cd0c21b2969f31b82eb683b26
Sander van de Graaf <mail@svdgraaf.nl>
2013-01-22 17:08:18 +0100

    fixed urls for old calls

--- tapatalk/urls.py
+++ tapatalk/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^xmlrpc/$', 'tapatalk.views.handle_xmlrpc', name='tapatalk-xmlrpc'),
+    url(r'^mobiquo.php$', 'tapatalk.views.handle_xmlrpc', name='tapatalk-xmlrpc-php'),
 )
