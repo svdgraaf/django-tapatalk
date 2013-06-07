b87455fdae0905256a722a7cf95e1e68481b9ece
Sander van de Graaf <mail@svdgraaf.nl>
2013-06-07 14:27:55 +0200

    made 3rd party tapatalk work a bit better

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ LICENSE = ''
 
 setup(
     name = 'django-tapatalk',
-    version = '0.5.3',
+    version = '0.5.4',
     description='DjangoBB Tapatalk implementation',
     long_description=README,
     author = 'Sander van de Graaf',
