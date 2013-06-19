fda211fd42fa4a3c6d805fbbd4039612695c580b
Sander van de Graaf <mail@svdgraaf.nl>
2013-06-19 11:01:41 +0200

    make sure we return utf8, as tapatalk only supports utf8 :(

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ LICENSE = ''
 
 setup(
     name = 'django-tapatalk',
-    version = '0.5.8.7',
+    version = '0.5.8.8',
     description='DjangoBB Tapatalk implementation',
     long_description=README,
     author = 'Sander van de Graaf',
